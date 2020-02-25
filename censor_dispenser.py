1e30cf3ee5257f80f92e57ab212c3528d3466ce8
FettKnight <fellipe.marcondes@gmail.com>
2020-02-25 20:11:20 +0000

    added proprietary terms and negative words lists

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -4,3 +4,6 @@ email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
+
+negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressing", "concerning", "horrible", "horribly", "questionable"]
